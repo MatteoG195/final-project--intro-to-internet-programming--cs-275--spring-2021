d92630ec18a477eef9c8b2b00edf029ee37f9b75
Matteo Gaudio <matteog1999@gmail.com>
2021-05-12 01:19:51 -0400

    Began work on setInterval, need testing, issues with div sizing

--- app/js/app.js
+++ app/js/app.js
@@ -12,39 +12,41 @@ let doDiamond = (size) => {
                 output = output + ` `;
             }
             //stars before center point
-            for(let j = half - Math.abs(i); j > 0; j--)
-            {
+            for(let j = half - Math.abs(i); j > 0; j--){
                 output = output + `*`;
             }
             //center point
             output = output + `*`;
             //stars after center point
-            for(let j = half - Math.abs(i); j > 0; j--)
-            {
+            for(let j = half - Math.abs(i); j > 0; j--){
                 output = output + `*`;
             }
             output = output + `\n`;
         }
     }else{
-        //top and bottom need a size-1 spaces and a single *.
+        //top and bottom need (size-1) spaces and a single *.
         for(let i = 0; i < size - 1; i++){
             output += ` `;
         }
         output += `*\n`;
+        //outer for loop needs to run from i = [-(half-1),half-1]
+        //because the outer rows (+-half) are resolved by the loops
+        //above and below
         for(let i = -1*half+1; i <= half-1; i++){
             //spaces before stars
             for(let j = 0; j < Math.abs(i); j++){
                 output = output + `  `;
             }
             //stars
-            for(let j = 0; j < size-2*Math.abs(i); j++)
-            {
+            for(let j = 0; j < size-2*Math.abs(i); j++){
+                //final * has a trailing space,
+                //but should be fine for now
                 output = output + `* `;
             }
             //add line break
             output = output + `\n`;
         }
-        //final row, need to place star in center
+        //bottom row, (size-1) spaces and a single *.
         for(let i = 0; i < size - 1; i++){
             output += ` `;
         }
@@ -59,4 +61,41 @@ window.onload = () => {
     console.log(doDiamond(5));
     console.log(doDiamond(4));
     console.log(doDiamond(6));
+
+    let div = document.getElementById(`diamondBox`);
+    let input;
+    let direction = `right`;
+
+    console.log(div.style.left);
+
+    const ONE_SECOND = 1000;
+
+    do{
+        input = window.prompt(`Enter a number for the size of the diamond.`);
+        input = parseInt(input);
+    }while(isNaN(input));
+
+    console.log(doDiamond(input));
+    div.content = doDiamond(input);
+
+    setInterval(() => {
+        let pos;
+
+        if(direction == `right`){
+            pos = div.style.right;
+            if(pos == window.clientWidth5){
+                direction = `left`;
+            }else{
+                div.style.left--;
+            }
+        }else{
+            pos == div.style.left;
+            if(pos == 0){
+                direction = `right`;
+            }else {
+                div.style.left++;
+            }
+        }
+        console.log(direction);
+    }, (ONE_SECOND))
 };
